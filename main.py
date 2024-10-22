5b9b8d4c5005c20a9569aabcd3725fa9678da136
Vsevolod Svetlichnyi <vsevolod.05@mail.ru>
2024-10-22 22:02:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,25 @@ def main(message):
     keyboard.add(key_site)
     bot.send_message(message.from_user.id, text="Choose the action", reply_markup=keyboard)
 
+@bot.message_handler(commands=['products'])
+def products(message):
+    keyboard = types.InlineKeyboardMarkup()
+    p1 = types.InlineKeyboardButton(text='1',callback_data='b1')
+    p2 = types.InlineKeyboardButton(text='2',callback_data='b2')
+    p3 = types.InlineKeyboardButton(text='3',callback_data='b3')
+    p4 = types.InlineKeyboardButton(text='4',callback_data='b4')
+    p5 = types.InlineKeyboardButton(text='5',callback_data='b5')
+    p6 = types.InlineKeyboardButton(text='6',callback_data='b6')
+    p7 = types.InlineKeyboardButton(text='7',callback_data='b7')
+    p8 = types.InlineKeyboardButton(text='8',callback_data='b8')
+    p9 = types.InlineKeyboardButton(text='9',callback_data='b9')
+    keyboard.add(p1,p2,p3,p4,p5,p6,p7,p8,p9)
+    bot.send_message(message.from_user.id, text="How many products you have?", reply_markup=keyboard)
+
+@bot.callback_query_handler(func=lambda callback: callback.data)
+def check_amount(callback):
+    bot.send_message(callback.message.chat.id, f'Write {callback.data[-1]} product(s) in singular form in one message. Example:')
+    bot.send_message(callback.message.chat.id, 'egg butter flour')
 
 @bot.message_handler(content_types=["text"]) #обработка текстового сообщения, введенного пользователем
 def get_text_message(message):
